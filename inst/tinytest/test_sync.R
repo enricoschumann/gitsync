cfcf3d6a9bb68ce6b4f65573d7ffd47c44ebae91
Enrico Schumann <es@enricoschumann.net>
2024-05-28 15:29:17 +0200

    Add tests

--- inst/tinytest/test_sync.R
+++ inst/tinytest/test_sync.R
@@ -1,15 +1,31 @@
+## create Machine1, with two repositories
 root <- file.path(tempdir(), "gitsync_tests")
-dir.create(file.path(root, "source"), recursive = TRUE)
-dir.create(file.path(root, "source", "A"), recursive = TRUE)
-
-setwd(file.path(root, "source", "A"))
-writeLines("test", "test.txt")
+dir.create(file.path(root, "Machine1", "D1"), recursive = TRUE)
+dir.create(file.path(root, "Machine1", "D", "D2"), recursive = TRUE)
+setwd(file.path(root, "Machine1", "D1"))
+writeLines("test D1 on machine 1", "test.txt")
 system2("git", "init")
 system2("git", c("add", "."))
 system2("git", c("commit", "-m", "'Initial commit'"))
+setwd(file.path(root, "Machine1", "D", "D2"))
+writeLines("test D2 in Machine1", "test.txt")
+system2("git", "init")
+system2("git", c("add", "."))
+system2("git", c("commit", "-m", "'Initial commit'"))
+## Machine1/
+## Machine1/
+## |-- D
+## |   `-- D2
+## |       `-- test.txt
+## `-- D1
+##     `-- test.txt
 
-setwd(file.path(root, "source"))
-## gitsync:::git_bundle_create(repos = "A",
-                            
 
-dir.create(file.path(root, "target"), recursive = TRUE)
+gitsync::git_paths(file.path(root, "Machine1"))
+gitsync::create_bundles(file.path(root, "Machine1"),
+                        bundle.dir = root)
+pull_from_bundles(file.path(root, "Machine2"),
+                  bundle.dir = root)
+pull_from_bundles(file.path(root, "Machine2"),
+                  bundle.dir = root,
+                  if.not.exists = "clone")
